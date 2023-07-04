a43b7a1f41be61db0557f671dce1ae80beaeacf8
Sergiu Ster <ssergiu@student.42heilbronn.de>
2023-07-04 09:20:27 +0200

    ex02

---
+++ ex02/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(void)
+{
+	std::string stringVAR = "HI THIS IS BRAIN";
+	std::string *stringPTR = &stringVAR;
+	std::string stringREF = *stringPTR;
+
+	std::cout<<&stringVAR<<std::endl;
+	std::cout<<stringPTR<<std::endl;
+	std::cout<<&stringREF<<std::endl;
+
+	std::cout<<stringVAR<<std::endl;
+	std::cout<<*stringPTR<<std::endl;
+	std::cout<<stringREF<<std::endl;
+	return (0);
+}
